b1efcd823a7fa4324f3e15ecc47e5eb2033a364b
lukas <lukas.heiligenbrunner@gmail.com>
2020-05-27 21:35:02 +0200

    delete delted movies from db and show db size

--- php/extractvideopreviews.php
+++ php/extractvideopreviews.php
@@ -7,6 +7,7 @@ $arr = scandir("../videos/prn/");
 
 $all = 0;
 $added = 0;
+$deleted = 0;
 $failed = 0;
 
 foreach ($arr as $elem) {
@@ -14,6 +15,8 @@ foreach ($arr as $elem) {
 
         $query = "SELECT * FROM videos WHERE movie_name = '" . mysqli_real_escape_string($conn, $elem) . "'";
         $result = $conn->query($query);
+
+        // insert if not available in db
         if (!mysqli_fetch_assoc($result)) {
             $pic = shell_exec("ffmpeg -hide_banner -loglevel panic -ss 00:04:00 -i \"../videos/prn/$elem\" -vframes 1 -q:v 2 -f singlejpeg pipe:1 2>/dev/null");
 
@@ -27,15 +30,58 @@ foreach ($arr as $elem) {
             if ($conn->query($query) === TRUE) {
                 echo('successfully added ' . $elem . " to video gravity\n");
                 $added++;
+                $all++;
             } else {
-                echo('{"data":"' . $conn->error . '"}');
+                echo('errored item: ' . $elem . "\n");
+                echo('{"data":"' . $conn->error . '"}\n');
                 $failed++;
             }
+        } else {
+            $all++;
         }
-        $all++;
     }
 }
 
+$query = "SELECT COUNT(*) as count FROM videos";
+$result = $conn->query($query);
+$r = mysqli_fetch_assoc($result);
+
+if ($all < $r['count']) {
+    echo "should be in gravity: " . $all . "\n";
+    echo "really in gravity: " . $r['count'] . "\n";
+    echo "cleaning up gravity\n";
+
+    $query = "SELECT movie_id,movie_url FROM videos";
+    $result = $conn->query($query);
+
+    while ($r = mysqli_fetch_assoc($result)) {
+        if (!file_exists("../" . $r['movie_url'])) {
+            $query = "DELETE FROM videos WHERE movie_id='" . $r['movie_id'] . "'";
+            if ($conn->query($query) === TRUE) {
+                echo('successfully deleted ' . $r['movie_url'] . " from video gravity\n");
+                $deleted++;
+            } else {
+                echo "failed to delete " . $r['movie_url'] . " from gravity: " . $conn->error . "\n";
+            }
+        }
+    }
+}
+
+// calculate size of databse here
+$size = -1;
+$query = "SELECT table_schema AS \"Database\", 
+                        ROUND(SUM(data_length + index_length) / 1024 / 1024, 3) AS \"Size\" 
+                        FROM information_schema.TABLES 
+                        WHERE TABLE_SCHEMA='hub'
+                        GROUP BY table_schema;";
+$result = $conn->query($query);
+if ($result->num_rows == 1) {
+    $row = $result->fetch_assoc();
+    $size = $row["Size"];
+}
+
 echo "Total gravity: " . $all . "\n";
+echo "Size of Databse is: " . $size . "MB\n";
 echo "added in this run: " . $added . "\n";
+echo "deleted in this run: " . $deleted . "\n";
 echo "errored in this run: " . $failed . "\n";
