f0007f44f35a69061bcd27479e50411ab660b2ca
Lukas Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2020-08-23 11:33:55 +0200

    do refactor of videoextraction to sperate class

--- api/extractvideopreviews.php
+++ api/extractvideopreviews.php
@@ -4,88 +4,14 @@ require_once './src/TMDBMovie.php';
 require_once './src/SSettings.php';
 require_once './src/VideoParser.php';
 
-$vp = new VideoParser();
-
 // allow UTF8 characters
 setlocale(LC_ALL, 'en_US.UTF-8');
 
+$vp = new VideoParser();
 $vp->writeLog("starting extraction!\n");
 
-
-echo("TMDB grabbing is " . ($TMDBenabled ? "" : "not") . " enabled \n");
+$sett = new SSettings();
 
 // load video path from settings
-$scandir = "../" . $settings->getVideoPath();
-$arr = scandir($scandir);
-
-$all = 0;
-$added = 0;
-$deleted = 0;
-$failed = 0;
-
-foreach ($arr as $elem) {
-    $ext = pathinfo($elem, PATHINFO_EXTENSION);
-    if ($ext == "mp4") {
-        $vp->processVideo($elem);
-    } else {
-        echo($elem . " does not contain a .mp4 extension! - skipping \n");
-        writeLog($elem . " does not contain a .mp4 extension! - skipping \n");
-    }
-}
-
-// auto cleanup db entries
-$query = "SELECT COUNT(*) as count FROM videos";
-$result = $conn->query($query);
-$r = mysqli_fetch_assoc($result);
-
-if ($all < $r['count']) {
-    echo "should be in gravity: " . $all . "\n";
-    $vp->writeLog("should be in gravity: " . $all . "\n");
-    echo "really in gravity: " . $r['count'] . "\n";
-    $vp->writeLog("really in gravity: " . $r['count'] . "\n");
-    echo "cleaning up gravity\n";
-    $vp->writeLog("cleaning up gravity\n");
-
-    $query = "SELECT movie_id,movie_url FROM videos";
-    $result = $conn->query($query);
-
-    while ($r = mysqli_fetch_assoc($result)) {
-        if (!file_exists("../" . $r['movie_url'])) {
-            $query = "SET foreign_key_checks = 0; DELETE FROM videos WHERE movie_id='" . $r['movie_id'] . "'";
-            if ($conn->query($query) === TRUE) {
-                echo('successfully deleted ' . $r['movie_url'] . " from video gravity\n");
-                writeLog('successfully deleted ' . $r['movie_url'] . " from video gravity\n");
-                $deleted++;
-            } else {
-                echo "failed to delete " . $r['movie_url'] . " from gravity: " . $conn->error . "\n";
-                writeLog("failed to delete " . $r['movie_url'] . " from gravity: " . $conn->error . "\n");
-            }
-        }
-    }
-}
-
-// calculate size of databse here
-$size = -1;
-$query = "SELECT table_schema AS \"Database\", 
-                        ROUND(SUM(data_length + index_length) / 1024 / 1024, 3) AS \"Size\" 
-                        FROM information_schema.TABLES 
-                        WHERE TABLE_SCHEMA='" . Database::getInstance()->getDatabaseName() . "'
-                        GROUP BY table_schema;";
-$result = $conn->query($query);
-if ($result->num_rows == 1) {
-    $row = $result->fetch_assoc();
-    $size = $row["Size"];
-}
-
-echo "Total gravity: " . $all . "\n";
-$vp->writeLog("Total gravity: " . $all . "\n");
-echo "Size of Databse is: " . $size . "MB\n";
-$vp->writeLog("Size of Databse is: " . $size . "MB\n");
-echo "added in this run: " . $added . "\n";
-$vp->writeLog("added in this run: " . $added . "\n");
-echo "deleted in this run: " . $deleted . "\n";
-$vp->writeLog("deleted in this run: " . $deleted . "\n");
-echo "errored in this run: " . $failed . "\n";
-$vp->writeLog("errored in this run: " . $failed . "\n");
-
-$vp->writeLog("-42"); // terminating characters to stop webui requesting infos
+$scandir = "../" . $sett->getVideoPath();
+$vp->extractVideos($scandir);
--- api/src/VideoParser.php
+++ api/src/VideoParser.php
@@ -6,7 +6,7 @@ require_once './src/SSettings.php';
 class VideoParser {
     private string $ffmpeg = 'ffmpeg'; //or: /usr/bin/ffmpeg , or /usr/local/bin/ffmpeg - depends on your installation (type which ffmpeg into a console to find the install path)
     private TMDBMovie $tmdb;
-// initial load of all available movie genres
+    // initial load of all available movie genres
     private $tmdbgenres;
 
     private $conn;
@@ -28,11 +28,89 @@ class VideoParser {
     }
 
 
+    public function extractVideos($foldername){
+        echo("TMDB grabbing is " . ($this->TMDBenabled ? "" : "not") . " enabled \n");
+        $arr = scandir($foldername);
+
+        $all = 0;
+        $added = 0;
+        $deleted = 0;
+        $failed = 0;
+
+        foreach ($arr as $elem) {
+            $ext = pathinfo($elem, PATHINFO_EXTENSION);
+            if ($ext == "mp4") {
+                $this->processVideo($elem);
+            } else {
+                echo($elem . " does not contain a .mp4 extension! - skipping \n");
+                $this->writeLog($elem . " does not contain a .mp4 extension! - skipping \n");
+            }
+        }
+
+        // auto cleanup db entries
+        $query = "SELECT COUNT(*) as count FROM videos";
+        $result = $this->conn->query($query);
+        $r = mysqli_fetch_assoc($result);
+
+        if ($all < $r['count']) {
+            echo "should be in gravity: " . $all . "\n";
+            $this->writeLog("should be in gravity: " . $all . "\n");
+            echo "really in gravity: " . $r['count'] . "\n";
+            $this->writeLog("really in gravity: " . $r['count'] . "\n");
+            echo "cleaning up gravity\n";
+            $this->writeLog("cleaning up gravity\n");
+
+            $query = "SELECT movie_id,movie_url FROM videos";
+            $result = $this->conn->query($query);
+
+            while ($r = mysqli_fetch_assoc($result)) {
+                if (!file_exists("../" . $r['movie_url'])) {
+                    $query = "SET foreign_key_checks = 0; DELETE FROM videos WHERE movie_id='" . $r['movie_id'] . "'";
+                    if ($this->conn->query($query) === TRUE) {
+                        echo('successfully deleted ' . $r['movie_url'] . " from video gravity\n");
+                        $this->writeLog('successfully deleted ' . $r['movie_url'] . " from video gravity\n");
+                        $deleted++;
+                    } else {
+                        echo "failed to delete " . $r['movie_url'] . " from gravity: " . $this->conn->error . "\n";
+                        $this->writeLog("failed to delete " . $r['movie_url'] . " from gravity: " . $this->conn->error . "\n");
+                    }
+                }
+            }
+        }
+
+        // calculate size of databse here
+        $size = -1;
+        $query = "SELECT table_schema AS \"Database\", 
+                        ROUND(SUM(data_length + index_length) / 1024 / 1024, 3) AS \"Size\" 
+                        FROM information_schema.TABLES 
+                        WHERE TABLE_SCHEMA='" . Database::getInstance()->getDatabaseName() . "'
+                        GROUP BY table_schema;";
+        $result = $this->conn->query($query);
+        if ($result->num_rows == 1) {
+            $row = $result->fetch_assoc();
+            $size = $row["Size"];
+        }
+
+        echo "Total gravity: " . $all . "\n";
+        $this->writeLog("Total gravity: " . $all . "\n");
+        echo "Size of Databse is: " . $size . "MB\n";
+        $this->writeLog("Size of Databse is: " . $size . "MB\n");
+        echo "added in this run: " . $added . "\n";
+        $this->writeLog("added in this run: " . $added . "\n");
+        echo "deleted in this run: " . $deleted . "\n";
+        $this->writeLog("deleted in this run: " . $deleted . "\n");
+        echo "errored in this run: " . $failed . "\n";
+        $this->writeLog("errored in this run: " . $failed . "\n");
+
+        $this->writeLog("-42"); // terminating characters to stop webui requesting infos
+    }
+
+
     public function processVideo($filename) {
         $moviename = substr($filename, 0, -4);
 
-        $query = "SELECT * FROM videos WHERE movie_name = '" . mysqli_real_escape_string($conn, $moviename) . "'";
-        $result = $conn->query($query);
+        $query = "SELECT * FROM videos WHERE movie_name = '" . mysqli_real_escape_string($this->conn, $moviename) . "'";
+        $result = $this->conn->query($query);
 
         // insert if not available in db
         if (!mysqli_fetch_assoc($result)) {
@@ -53,7 +131,7 @@ class VideoParser {
             }
 
             // extract poster from video
-            $backpic = shell_exec("ffmpeg -hide_banner -loglevel panic -ss 00:04:00 -i \"../videos/prn/$elem\" -vframes 1 -q:v 2 -f singlejpeg pipe:1 2>/dev/null");
+            $backpic = shell_exec("ffmpeg -hide_banner -loglevel panic -ss 00:04:00 -i \"../videos/prn/$filename\" -vframes 1 -q:v 2 -f singlejpeg pipe:1 2>/dev/null");
             // convert video to base64
             $backpic64 = 'data:image/jpeg;base64,' . base64_encode($backpic);
 
