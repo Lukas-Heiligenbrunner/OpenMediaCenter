3eb908ebff9eed3c18d9fc4676162b377016cb02
Lukas Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2020-10-06 00:04:54 +0200

    add drag and drop support for addtagpopup esc closes the popup theme style is used

--- src/elements/AddTagPopup/AddTagPopup.js
+++ src/elements/AddTagPopup/AddTagPopup.js
@@ -3,21 +3,31 @@ import ReactDom from 'react-dom';
 import style from './AddTagPopup.module.css'
 import Tag from "../Tag/Tag";
 import {Line} from "../PageTitle/PageTitle";
+import GlobalInfos from "../../GlobalInfos";
 
 /**
  * component creates overlay to add a new tag to a video
  */
 class AddTagPopup extends React.Component {
+    element;
     constructor(props, context) {
         super(props, context);
 
         this.state = {items: []};
         this.handleClickOutside = this.handleClickOutside.bind(this);
+        this.keypress = this.keypress.bind(this);
+
         this.props = props;
     }
 
     componentDidMount() {
         document.addEventListener('click', this.handleClickOutside);
+        document.addEventListener('keyup', this.keypress);
+
+        // add element drag drop events
+        if(this.element != null){
+            this.dragElement();
+        }
 
         const updateRequest = new FormData();
         updateRequest.append('action', 'getAllTags');
@@ -47,9 +57,10 @@ class AddTagPopup extends React.Component {
     }
 
     render() {
+        const themeStyle = GlobalInfos.getThemeStyle();
         return (
-            <div className={style.popup}>
-                <div className={style.header}>Add a Tag to this Video:</div>
+            <div className={[style.popup, themeStyle.thirdbackground].join(' ')} ref={el => this.element = el }>
+                <div className={[style.header, themeStyle.textcolor].join(' ')}>Add a Tag to this Video:</div>
                 <Line/>
                 <div className={style.content}>
                     {this.state.items ?
@@ -59,11 +70,15 @@ class AddTagPopup extends React.Component {
                             }}>{i.tag_name}</Tag>
                         )) : null}
                 </div>
-
             </div>
         );
     }
 
+    /**
+     * add a new tag to this video
+     * @param tagid tag id to add
+     * @param tagname tag name to add
+     */
     addTag(tagid, tagname) {
         console.log(this.props)
         const updateRequest = new FormData();
@@ -82,6 +97,58 @@ class AddTagPopup extends React.Component {
                     }
                     this.props.onHide();
                 }));
+    }
+
+    /**
+     * key event handling
+     * @param event keyevent
+     */
+    keypress(event) {
+        // hide if escape is pressed
+        if (event.key === "Escape"){
+            this.props.onHide();
+        }
+    }
+
+    /**
+     * make the element drag and droppable
+     */
+    dragElement() {
+        let pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+
+        console.log(this.props);
+
+        const elmnt = this.element;
+        elmnt.firstChild.onmousedown = dragMouseDown;
+
+
+        function dragMouseDown(e) {
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e.preventDefault();
+            // calculate the new cursor position:
+            pos1 = pos3 - e.clientX;
+            pos2 = pos4 - e.clientY;
+            pos3 = e.clientX;
+            pos4 = e.clientY;
+            // set the element's new position:
+            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+        }
+
+        function closeDragElement() {
+            // stop moving when mouse button is released:
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
     }
 }
 
