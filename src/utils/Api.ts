7f67146510d7d62776dfb0fa964d4018de19f252
lukas <lukas.heiligenbrunner@gmail.com>
2020-12-16 22:15:16 +0100

    add doc for Api module

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -1,9 +1,9 @@
 let customBackendURL: string;
 
-function getAPIDomain(): string {
-    return getBackendDomain() + '/api/';
-}
-
+/**
+ * get the domain of the api backend
+ * @return string domain of backend http://x.x.x.x/bla
+ */
 export function getBackendDomain(): string {
     let userAgent = navigator.userAgent.toLowerCase();
     if (userAgent.indexOf(' electron/') > -1) {
@@ -19,16 +19,34 @@ export function getBackendDomain(): string {
     }
 }
 
+/**
+ * set a custom backend domain
+ * @param domain a url in format [http://x.x.x.x/somanode]
+ */
 export function setCustomBackendDomain(domain: string) {
     customBackendURL = domain;
 }
 
+/**
+ * a helper function to get the api path
+ */
+function getAPIDomain(): string {
+    return getBackendDomain() + '/api/';
+}
+
+/**
+ * interface how an api request should look like
+ */
 interface ApiBaseRequest {
     action: string,
 
     [_: string]: string
 }
 
+/**
+ * helper function to build a formdata for requesting post data correctly
+ * @param args api request object
+ */
 function buildFormData(args: ApiBaseRequest): FormData {
     const req = new FormData();
 
@@ -38,6 +56,13 @@ function buildFormData(args: ApiBaseRequest): FormData {
     return req;
 }
 
+/**
+ * A backend api call
+ * @param apinode which api backend handler to call
+ * @param fd the object to send to backend
+ * @param callback the callback with json reply from backend
+ * @param errorcallback a optional callback if an error occured
+ */
 export function callAPI(apinode: string, fd: ApiBaseRequest, callback: (_: object) => void, errorcallback: (_: object) => void = (_: object) => {
 }): void {
     fetch(getAPIDomain() + apinode, {method: 'POST', body: buildFormData(fd)})
@@ -47,6 +72,12 @@ export function callAPI(apinode: string, fd: ApiBaseRequest, callback: (_: objec
             })).catch(reason => errorcallback(reason));
 }
 
+/**
+ * A backend api call
+ * @param apinode which api backend handler to call
+ * @param fd the object to send to backend
+ * @param callback the callback with PLAIN text reply from backend
+ */
 export function callAPIPlain(apinode: string, fd: ApiBaseRequest, callback: (_: any) => void): void {
     fetch(getAPIDomain() + apinode, {method: 'POST', body: buildFormData(fd)})
         .then((response) => response.text()
