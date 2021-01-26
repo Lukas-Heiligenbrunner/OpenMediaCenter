ac126f6a9dfcf4c8ef1fe85893cd2ca2ec8e4b9c
Lukas Heiligenbrunner <lukas.heiligenbrunner@gmail.com>
2021-01-26 19:14:57 +0000

    make tags deleteable seperate sidebar for each different category page

---
+++ src/elements/Popups/SubmitPopup/SubmitPopup.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import PopupBase from '../PopupBase';
+import {Button} from '../../GPElements/Button';
+
+interface props {
+    onHide: (_: void) => void;
+    submit: (_: void) => void;
+}
+
+export default function SubmitPopup(props: props): JSX.Element {
+    return (
+        <PopupBase title='Are you sure?' onHide={props.onHide} height='160px' width='300px'>
+            <Button title='Submit' color={{backgroundColor: 'green'}} onClick={(): void => props.submit()}/>
+            <Button title='Cancel' color={{backgroundColor: 'red'}} onClick={(): void => props.onHide()}/>
+        </PopupBase>
+    );
+
+}
