ac1872c5c6fa14084b769674589acfa99e35d1ff
lukas <lukas.heiligenbrunner@gmail.com>
2021-02-04 18:39:20 +0100

    add database connection in go

---
+++ apiGo/database/Database.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func InitDB() {
+	// Open up our database connection.
+	// I've set up a database on my local machine using phpmyadmin.
+	// The database is called testDb
+	var err error
+	db, err = sql.Open("mysql", "root:1qayxsw2@tcp(192.168.0.30:3306)/hub")
+
+	// if there is an error opening the connection, handle it
+	if err != nil {
+		fmt.Println("failed to connect...")
+		panic(err.Error())
+	}
+}
+
+func Query(SQL string) *sql.Rows {
+	// perform a db.Query insert
+	query, err := db.Query(SQL)
+
+	// if there is an error inserting, handle it
+	if err != nil {
+		panic(err.Error())
+	}
+	// be careful deferring Queries if you are using transactions
+	defer query.Close()
+
+	return query
+}
+
+func Close() {
+	db.Close()
+}
--- apiGo/main.go
+++ apiGo/main.go
@@ -4,11 +4,15 @@ import (
 	"encoding/json"
 	"fmt"
 	"openmediacenter/apiGo/api"
+	"openmediacenter/apiGo/database"
 	"os"
 )
 
 func main() {
 	fmt.Println("init OpenMediaCenter server")
+
+	database.InitDB()
+	defer database.Close()
 
 	type name struct {
 		Action string
@@ -19,6 +23,12 @@ func main() {
 	api.AddHandler("getMovies", &testiii, func() []byte {
 		fmt.Println(testiii.Action)
 		fmt.Println(testiii.Tag)
+
+		rows := database.Query("SELECT * FROM videos")
+
+		for rows.Next() {
+
+		}
 
 		type VideoUnloadedType struct {
 			Movie_id   int
